eb9384e17b2f9465cef770557d6980750447ff94
Cinnayanny <jenniferwood2005@gmail.com>
2023-08-18 14:02:00 +1000

    Updated main mockup

--- index.php
+++ index.php
@@ -8,10 +8,36 @@
 So, 1.5, 9, 1.5 or 1, 9, 2-->
 <div class="container-fluid">
     <div class="row">
-        <div class="col-1 bg-light p-3 border">
-            Communities n stuff
-            <!-- Tested around with making columns inside these containers. Nothing I tried (admittedly not a lot) worked -->
+        <div class="col">
+            <div class="col bg-light p-1 border">
+                <p><u><b><center>Trending Communities</center></b></u></p>
+                <ul>
+                    <li>Community</li>
+                    <li>Community</li>
+                    <li>Community</li>
+                    <li>Community</li>
+                    <li>Community</li>
+                    <li>Community</li>
+                    <li>Community</li>
+                    <li>Community</li>
+                </ul>
+                <!-- Tested around with making columns inside these containers. Nothing I tried (admittedly not a lot) worked -->
+            </div>
+            <div class="col bg-light p-1 border">
+                <p><u><b><center>Hot Topics</center></b></u></p>
+                <ul>
+                    <li>Topic</li>
+                    <li>Topic</li>
+                    <li>Topic</li>
+                    <li>Topic</li>
+                    <li>Topic</li>
+                    <li>Topic</li>
+                    <li>Topic</li>
+                    <li>Topic</li>
+                </ul>
+            </div>
         </div>
+        <!-- If someone could find a way to have separation between the columns, that would be great -->
         <!-- Currently, these sit in the centre rather than touching the edges as I would have liked -->
         <div class="col-9 bg-light p-3 border">
             The feed
