3262b422030a46d70ebc8c8c874f4d231191ce85
Cinnayanny <jenniferwood2005@gmail.com>
2023-08-18 14:21:39 +1000

    Removed "Upload Post" option from navbar. If we already have "Make Post", I don't see the need for the other

--- template.php
+++ template.php
@@ -23,7 +23,7 @@
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="contact.php">Contact Us</a></li>
-                <li class="nav-item"><a class="nav-link" href="Test%20post%20upload.php">Upload Post</a></li>
+<!--                <li class="nav-item"><a class="nav-link" href="Test%20post%20upload.php">Upload Post</a></li>-->
                 </div>
 
                 <?php
@@ -74,21 +74,21 @@
 <!--https://getbootstrap.com/docs/5.0/layout/containers/
 15%, 70%, 15% or 10%, 70%, 20%
 So, 1.5, 9, 1.5 or 1, 9, 2-->
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-1 bg-light p-3 border">
-            Communities n stuff
-            <!-- Tested around with making columns inside these containers. Nothing I tried (admittedly not a lot) worked -->
-        </div>
-        <!-- Currently, these sit in the centre rather than touching the edges as I would have liked -->
-        <div class="col-9 bg-light p-3 border">
-            The feed
-        </div>
-        <div class="col-2 bg-light p-3 border">
-            User stuff
-        </div>
-    </div>
-</div>
+<!--<div class="container-fluid">-->
+<!--    <div class="row">-->
+<!--        <div class="col-1 bg-light p-3 border">-->
+<!--            Communities n stuff-->
+<!--            <!-- Tested around with making columns inside these containers. Nothing I tried (admittedly not a lot) worked -->
+<!--        </div>-->
+<!--        <!-- Currently, these sit in the centre rather than touching the edges as I would have liked -->
+<!--        <div class="col-9 bg-light p-3 border">-->
+<!--            The feed-->
+<!--        </div>-->
+<!--        <div class="col-2 bg-light p-3 border">-->
+<!--            User stuff-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 
 
 
